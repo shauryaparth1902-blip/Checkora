a42cd255c841facc48acedcda0e3ee97fb933e18
tharunika-19 <tharunikabodasu93.6@gmail.com>
2026-05-18 14:56:44 +0530

    resolve merge conflict in auth.css

--- core/settings.py
+++ core/settings.py
@@ -135,7 +135,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
 
 # Email Configuration for OTP
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
